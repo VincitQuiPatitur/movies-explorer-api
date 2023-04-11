fc54e0e172468f4671e9d75c2794d73e2dbacf52
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-04-11 19:29:06 +0300

    feat: add models

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const app = express();
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
-})
+});
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 30,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: (v) => isEmail(v),
+        message: 'Неправильный формат почты',
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+module.exports = mongoose.model('user', userSchema);
